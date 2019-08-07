c7f711e24244c06c7318cbb388c0446b5c094f10
Alejandro Jiménez Rico <aljrico@gmail.com>
2019-08-07 06:40:02 +0000

    version 0.1.0

---
+++ R/data.R
@@ -0,0 +1,7 @@
+
+#' "Planets"
+#'
+#' A data set containing the most basic information available of all known planets.
+#'
+#' @format A data frame with 3915 observations of 12 variables
+"planets"
---
+++ R/planets.R
@@ -0,0 +1,14 @@
+#' Basic information of known Planets
+#'
+#' This package contains basic information from all knwon Planets
+#' @docType package
+#' @name planets
+#'
+#' @examples
+#' data(planets)
+#'
+#' plot(
+#' data = planets,
+#' `Mass (MJ)` ~ `Temperature (K)`
+#' )
+NULL
